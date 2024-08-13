949087a24e13a03015b94f3b8ff4048dd8273c68
andrea <ngocanhhsgs@gmail.com>
2024-08-13 10:30:29 +0300

    update information

--- components/intro.tsx
+++ components/intro.tsx
@@ -66,7 +66,7 @@ export default function Intro() {
       >
         <span className="font-bold">Moi !!! I'm Andrea.</span> I'm a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">more than 2 years</span> of experience. I
+        <span className="font-bold">more than 3 years</span> of experience. I
         enjoy building <span className="italic">sites & apps</span>.
       </motion.h1>
 
